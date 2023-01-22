306a57529d8e702d5b1b22465f5e1557bab91621
Juan <rigualjuan@gmail.com>
2023-01-22 19:04:37 -0300

    Create server to to simulate SSR with styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./pages/About";
+import Articles from "./pages/Articles";
+import Home from "./pages/Home";
+import { Switch, Route, Link } from "react-router-dom";
+import styled from "styled-components";
 
-function App() {
+const GreenHeading = styled.h1`
+  color: green;
+  font-size: 1.5rem;
+`;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GreenHeading>Server-Side render Example</GreenHeading>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/articles">Articles</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
-
-export default App;
